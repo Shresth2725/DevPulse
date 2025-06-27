3ea7af672692fb8b1cb6191cb115b1409f83ae14
Shresth <2725shresth@gmail.com>
2025-06-27 11:20:35 +0530

    Add get APIs

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 
+// POST: dynamic signup api
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
@@ -14,6 +15,30 @@ app.post("/signup", async (req, res) => {
     res.send("User add succesfully");
   } catch (err) {
     res.status(400).send("Error saving the user: " + err.message);
+  }
+});
+
+// GET: get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("user not found");
+    }
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+// GET: get all user data api(profile)
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
