7ab3e864d6e8136860fca2de21debb14ee696405
Shresth <2725shresth@gmail.com>
2025-07-06 18:46:32 +0530

    Added Feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const connectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 
@@ -76,6 +77,45 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
     });
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+// GET: fetch feed for user
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    // User should see all user except
+    // 0. his own card
+    // 1. his connection
+    // 2. ignored people
+    // 3. whom already interested in
+
+    const loggedInUser = req.user;
+    const connectionRequest = await connectionRequestModel
+      .find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      })
+      .select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequest.forEach((user) => {
+      hideUsersFromFeed.add(user.fromUserId.toString());
+      hideUsersFromFeed.add(user.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    });
+
+    if (!users) {
+      throw new Error("No user left");
+    }
+
+    res.send(users);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
