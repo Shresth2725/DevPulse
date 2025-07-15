5fd8e927ecc9be258d1bd4ddc8a36971a5e282fc
Shresth <2725shresth@gmail.com>
2025-07-15 16:28:14 +0530

    bug fix

--- src/models/user.js
+++ src/models/user.js
@@ -45,7 +45,7 @@ const userSchema = new mongoose.Schema(
     gender: {
       type: String,
       validate(value) {
-        if (!["male", "female", "others"].includes(value)) {
+        if (!["male", "female", "others", ""].includes(value)) {
           throw new Error("Gender not valid");
         }
       },
