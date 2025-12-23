9ed1fe37893cdc91d187d80c4e76f82436fc3955
Shresth <152704478+Shresth2725@users.noreply.github.com>
2025-12-23 16:32:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("expresss");
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
