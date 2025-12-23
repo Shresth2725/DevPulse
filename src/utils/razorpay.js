fc3546ef405e7e3013ca2ffbc8fbef049a03bc6a
Shresth <152704478+Shresth2725@users.noreply.github.com>
2025-12-23 20:22:18 +0530

    Refactor Razorpay import and initialization

--- src/utils/razorpay.js
+++ src/utils/razorpay.js
@@ -1,13 +1,15 @@
-import Razorpay from "razorpay";
+const Razorpay = require("razorpay");
 
-let razorpayInstance;
+let razorpayInstance = null;
 
-export function getRazorpay() {
+function getRazorpay() {
   if (!razorpayInstance) {
     razorpayInstance = new Razorpay({
       key_id: process.env.RAZORPAY_KEY_ID,
-      key_secret: process.env.RAZORPAY_SECRET_KEY
+      key_secret: process.env.RAZORPAY_SECRET_KEY,
     });
   }
   return razorpayInstance;
 }
+
+module.exports = { getRazorpay };
